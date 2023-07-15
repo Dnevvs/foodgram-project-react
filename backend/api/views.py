29cd88d705be1f154d9fe9a9d3315e0f0804eeb4
Dnevvs <dnevvs@yandex.ru>
2023-07-15 12:46:01 +0300

    Diploma 5

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-from api.pagination import MyPagination
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
